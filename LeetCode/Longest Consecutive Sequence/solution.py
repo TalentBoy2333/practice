510685e4e00fd3548f1addf604a4db8cb9c94040
TalentBoy233 <957498562@qq.com>
2019-06-05 17:28:43 +0800

    update 4

--- LeetCode/Longest Consecutive Sequence/solution.py
+++ LeetCode/Longest Consecutive Sequence/solution.py
@@ -26,8 +26,8 @@ class Solution(object):
                 max_len = cur_len 
         return max_len 
 
-nums = [100, 4, 200, 1, 3, 2]
-so = Solution()
-res = so.longestConsecutive(nums)
-print(res)
+# nums = [100, 4, 200, 1, 3, 2]
+# so = Solution()
+# res = so.longestConsecutive(nums)
+# print(res)
                 
--- LeetCode/Longest Consecutive Sequence/test.py
+++
@@ -1,4 +0,0 @@
-a = {1:2, 3:2}
-print(a)
-a.pop(1)
-print(a)
